c1592412423df48ac40b7c1d70a0ba868b9f7307
CS <CS@qq.com>
2020-04-09 22:30:22 +0800

    论文投稿管理。论文投稿管理的流程还未配置

---
+++ CS.BLL/SR/SR_PAPER_CONTRIBUTE.cs
@@ -0,0 +1,137 @@
+﻿using CS.Library.BaseQuery;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS.BLL.SR
+{
+    /// <summary>
+    /// 论文投稿
+    /// </summary>
+    public class SR_PAPER_CONTRIBUTE : BBaseQuery
+    {
+        /// <summary>
+        /// 单例
+        /// </summary>
+        public static SR_PAPER_CONTRIBUTE Instance = new SR_PAPER_CONTRIBUTE();
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        public SR_PAPER_CONTRIBUTE()
+        {
+            this.IsAddIntoCache = true;
+            this.TableName = "SR_PAPER_CONTRIBUTE";
+            this.ItemName = "论文投稿";
+            this.KeyField = "ID";
+            this.OrderbyFields = "ID";
+        }
+
+        #region 实体
+        /// <summary>
+        /// 实体
+        /// </summary>
+        public class Entity
+        {
+            /// <summary>
+            /// ID 
+            /// </summary>
+            [Field(IsPrimaryKey = true, IsAutoIncrement = true, IsNotNull = true, Comment = "ID ")]
+            public int ID { get; set; }
+
+            /// <summary>
+            /// 论文名称
+            /// </summary>
+            [Field(IsNotNull = true, Comment = "论文名称")]
+            public string NAME { get; set; }
+
+            /// <summary>
+            /// 文章类型
+            /// </summary>
+            [Field(IsNotNull = true, Comment = "文章类型")]
+            public int ARTICLE_TYPE_ID { get; set; }
+
+            /// <summary>
+            /// 课题ID
+            /// </summary>
+            [Field(IsNotNull = true, Comment = "课题ID")]
+            public int TOPIC_ID { get; set; }
+
+            /// <summary>
+            /// 学科
+            /// </summary>
+            [Field(IsNotNull = true, Comment = "学科")]
+            public int SUBJECT_ID { get; set; }
+
+            /// <summary>
+            /// 投稿人
+            /// </summary>
+            [Field(IsNotNull = true, Comment = "投稿人")]
+            public string CONTRIBUTOR { get; set; }
+
+            /// <summary>
+            /// 联系电话
+            /// </summary>
+            [Field(IsNotNull = true, Comment = "联系电话")]
+            public string PHONE { get; set; }
+
+            /// <summary>
+            /// 论文内容
+            /// </summary>
+            [Field(IsNotNull = true, Comment = "论文内容")]
+            public string CONTENT { get; set; }
+
+            /// <summary>
+            /// 附件IDS(编号以英文逗号分隔)
+            /// 使用附件控件自动存储
+            /// </summary>
+            [Field(IsNotNull = false, Comment = "附件IDS")]
+            public string FILES { get; set; }
+
+            #region 操作人
+            /// <summary>
+            /// 创建时间
+            /// </summary>
+            [Field(IsNotNull = true, Comment = "创建时间")]
+            public DateTime CREATE_TIME { get; set; }
+
+            /// <summary>
+            /// 更新时间
+            /// </summary>
+            [Field(IsNotNull = true, Comment = "更新时间")]
+            public DateTime UPDATE_TIME { get; set; }
+
+            /// <summary>
+            /// 创建人
+            /// </summary>
+            [Field(IsNotNull = true, Comment = "创建人")]
+            public int CREATE_UID { get; set; }
+
+            /// <summary>
+            /// 修改人
+            /// </summary>
+            [Field(IsNotNull = true, Comment = "修改人")]
+            public int UPDATE_UID { get; set; }
+            #endregion
+
+            #region 审批状态
+            /// <summary>
+            /// 审批状态
+            /// </summary>
+            [Field(IsNotNull = true, DefaultValue = "0", Comment = "审批状态")]
+            public int FLOW_STATE { get; set; }
+
+            /// <summary>
+            /// 是否审批通过
+            /// </summary>
+            [Field(IsNotNull = true, DefaultValue = "0", Comment = "是否审批通过")]
+            public int IS_ADOPT { get; set; }
+            #endregion
+        }
+        #endregion
+
+    }
+}
---
+++ CS.WebUI/Controllers/SR/SrPaperContributeController.cs
@@ -0,0 +1,185 @@
+﻿
+using CS.Base.Log;
+using CS.BLL.FW;
+using CS.Common.FW;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CS.WebUI.Models.FW;
+using CS.BLL.SR;
+using System.Text;
+using CS.Base.DBHelper;
+using System.Text.RegularExpressions;
+
+namespace CS.WebUI.Controllers.FW
+{
+    /// <summary>
+    /// 论文投稿
+    /// </summary>
+    public class SrPaperContributeController : ABaseController
+    {
+        public string Modular = "论文投稿";
+
+        #region 论文投稿管理
+        public ActionResult FlowEdit(int id = 0)
+        {
+            SR_PAPER_CONTRIBUTE.Entity paper = new SR_PAPER_CONTRIBUTE.Entity();
+
+            #region 01.论文备案主体
+            if (id > 0)
+            {
+                paper = SR_PAPER_CONTRIBUTE.Instance.GetEntityByKey<SR_PAPER_CONTRIBUTE.Entity>(id);
+            }
+            #endregion
+
+            #region 02.课题下拉(树状结构) 考虑：显示属于当前人员的课题
+            //课题下拉树
+            var topic = SR_TOPIC.Instance.GetEntityByKey<SR_TOPIC.Entity>(paper.TOPIC_ID);
+            ViewBag.TopicName = topic.NAME;
+            #endregion
+
+            #region 03.学科
+            var sub = SR_SUBJECT.Instance.GetEntityByKey<SR_SUBJECT.Entity>(paper.SUBJECT_ID);
+            ViewBag.SubjectName = sub.NAME;
+            #endregion
+
+            #region 04.文章类型
+            var artype = SR_ARTICLE_TYPE.Instance.GetEntityByKey<SR_ARTICLE_TYPE.Entity>(paper.ARTICLE_TYPE_ID);
+            ViewBag.ArticleName = artype.NAME;
+            #endregion
+
+            return View(paper);
+        }
+        /// <summary>
+        /// 编辑及新增
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult Edit(int id = 0)
+        {
+            SR_PAPER_CONTRIBUTE.Entity paper = new SR_PAPER_CONTRIBUTE.Entity();
+
+            #region 01.论文投稿主体
+            if (id > 0)
+            {
+                paper = SR_PAPER_CONTRIBUTE.Instance.GetEntityByKey<SR_PAPER_CONTRIBUTE.Entity>(id);
+            }
+            #endregion
+
+            #region 02.课题下拉(树状结构) 考虑：显示属于当前人员的课题
+            var list = SR_TOPIC.Instance.GetTopicTree();
+            if (list != null && list.Count > 0)
+            {
+                foreach (var item in list)
+                {
+                    item.icon = ApplicationPath + item.icon;
+                }
+            }
+            //课题下拉树
+            ViewBag.TopicSelect = SerializeObject(list);
+            #endregion
+
+            #region 03.学科
+            DataTable dt = SR_SUBJECT.Instance.GetTable();
+            if (dt != null && dt.Rows.Count > 0)
+            {
+                var obj = new List<object>();
+                foreach (DataRow dr in dt.Rows)
+                {
+                    obj.Add(
+                        new
+                        {
+                            id = Convert.ToInt32(dr["ID"]),
+                            pId = string.IsNullOrWhiteSpace(dr["PARENT_ID"].ToString()) ? "" : dr["PARENT_ID"],
+                            name = dr["NAME"].ToString(),
+                            value = Convert.ToInt32(dr["ID"])
+                        });
+                }
+                ViewBag.SubjectSelect = SerializeObject(obj);
+            }
+            #endregion
+
+            #region 04.文章类型
+            DataTable dtArticleType = SR_ARTICLE_TYPE.Instance.GetTable();
+            if (dtArticleType != null && dtArticleType.Rows.Count > 0)
+            {
+                var obj = new List<object>();
+                foreach (DataRow dr in dtArticleType.Rows)
+                {
+                    obj.Add(
+                        new
+                        {
+                            id = Convert.ToInt32(dr["ID"]),
+                            pId = string.IsNullOrWhiteSpace(dr["PARENT_ID"].ToString()) ? "" : dr["PARENT_ID"],
+                            name = dr["NAME"].ToString(),
+                            value = Convert.ToInt32(dr["ID"])
+                        });
+                }
+                ViewBag.ArticleTypeSelect = SerializeObject(obj);
+            }
+            #endregion
+
+            return View(paper);
+        }
+
+        /// <summary>
+        /// 编辑提交(暂时未考虑审核)
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [ValidateInput(false)]
+        public JsonResult Edit(SR_PAPER_CONTRIBUTE.Entity ent, FormCollection collection)
+        {
+            JsonResultData result = new JsonResultData();
+
+            try
+            {
+                //throw new Exception("抛出错误");
+                #region 00.数据校验(暂未实现)
+
+                #endregion
+
+                #region 01.保存论文投稿
+                int entId = ent.ID;
+                ent.UPDATE_TIME = DateTime.Now;
+                ent.UPDATE_UID = SystemSession.UserID;
+                if (ent.ID == 0)
+                {
+                    entId = SR_PAPER_CONTRIBUTE.Instance.GetNextValueFromSeqDef();
+                    ent.CREATE_TIME = DateTime.Now;
+                    ent.CREATE_UID = SystemSession.UserID;
+                    ent.ID = entId;
+                    SR_PAPER_CONTRIBUTE.Instance.Add(ent);
+                }
+                else
+                {
+                    var oldEnt = SR_PAPER_CONTRIBUTE.Instance.GetEntityByKey<SR_PAPER_CONTRIBUTE.Entity>(ent.ID);
+                    ent.CREATE_TIME = oldEnt.CREATE_TIME;
+                    ent.CREATE_UID = oldEnt.CREATE_UID;
+                    SR_PAPER_CONTRIBUTE.Instance.UpdateByKey(ent, ent.ID);
+                }
+                #endregion
+
+                result.IsSuccess = true;
+                result.Result = entId.ToString();
+                result.Message = string.Format(@"论文投稿上报成功!");
+            }
+            catch (Exception ex)
+            {
+                string err = string.Format(@"论文投稿上报失败：{0}", ex.Message);
+                result.IsSuccess = false;
+                result.Message = err;
+                BLog.Write(BLog.LogLevel.ERROR, err);
+                WriteOperationLog(BLog.LogLevel.ERROR, false, Modular, "论文投稿上报", "", err);
+            }
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+        #endregion
+
+    }
+}
